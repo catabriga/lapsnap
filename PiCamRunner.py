2f3688f5ee17f6a96746fb81b5af4a6bd04c36a0
Lucas Catabriga <catabriga90@gmail.com>
2021-01-23 16:03:28 +0100

    Add cam runner

---
+++ PiCamRunner.py
@@ -0,0 +1,35 @@
+import picamera
+import picamera.array
+import time
+
+class PiCamRunner():
+    def __init__(self, timelapse_period, timelapse_callback, size, rotation, framerate):
+        self.timelapse_period = timelapse_period
+        self.timelapse_callback = timelapse_callback
+        self.size = size
+        self.framerate = framerate
+        self.rotation = rotation
+
+
+    def run(self):
+        camera = picamera.PiCamera()
+        camera.resolution = self.size
+        camera.framerate = self.framerate
+        camera.rotation = self.rotation
+
+        raw_capture = picamera.array.PiRGBArray(camera, size=camera.resolution)
+
+        last_time = time.time()
+
+        for frame in camera.capture_continuous(raw_capture, format="rgb", use_video_port=True):
+
+            current_time = time.time()
+
+            image = frame.array
+
+            if(current_time - last_time > self.timelapse_period):
+                self.timelapse_callback(image)
+                last_time = current_time
+
+            raw_capture.truncate(0)
+
---
+++ lapsnap.py
@@ -0,0 +1,60 @@
+import os
+import time
+import datetime
+import sys
+import argparse
+import signal
+import numpy as np
+
+import PiCamRunner
+
+class Lapsnap():
+    def __init__(self, root_image_folder, period, size, rotation, framerate):
+        self.root_image_folder = root_image_folder
+        self.period = period
+        self.size = tuple(size)
+        self.rotation = rotation
+        self.framerate = framerate
+
+    def get_day_folder(self, root_folder):
+        today = str(datetime.date.today())
+        full_path = os.path.join(root_folder, today)
+        os.makedirs(full_path, exist_ok=True)
+        return full_path
+
+    def save_image(self, image):
+        folder = self.get_day_folder(self.root_image_folder)
+        name = os.path.join(folder, str(datetime.datetime.now()) + '.jpg')
+        image.save(name)
+
+    def timelapse_callback(self, image):
+        self.save_image(image)
+
+    def run(self):
+        picam_detector = PiCamRunner.PiCamRunner(timelapse_period=self.period,
+                                                 timelapse_callback=self.timelapse_callback,
+                                                 size=self.size,
+                                                 rotation=self.rotation,
+                                                 framerate=self.framerate)
+
+        picam_detector.run()
+
+
+def signal_handler():
+    raise
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save_folder', default="./lapsnap_data/", help='path to save images')
+    parser.add_argument('--period', default=60, help='period between saved images in seconds')
+    parser.add_argument('--size', nargs=2, default=[640,480], help='image resolution')
+    parser.add_argument('--rotation', default=0, help='image rotation')
+    parser.add_argument('--framerate', default=10, help='capture framerate')
+    args = parser.parse_args()
+
+    signal.signal(signal.SIGTERM, signal_handler)
+
+    lapsnap = Lapsnap(root_image_folder=args.save_folder, period=args.period, size=args.size,
+                      rotation=args.rotation, framerate=args.framerate)
+    lapsnap.run()
