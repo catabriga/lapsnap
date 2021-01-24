fba2ac340abe89ec4d4307f6d8269790a1483220
lucas <catabriga90@gmail.com>
2021-01-24 11:42:48 +0100

    change exposure

--- PiCamRunner.py
+++ PiCamRunner.py
@@ -19,8 +19,8 @@ class PiCamRunner():
             camera.shutter_speed = 6000000
             camera.exposure_mode = 'night'
             camera.iso = 800
-            camera.exposure_compensation = -12
-            camera.contrast = 50
+            camera.exposure_compensation = 12
+            camera.contrast = 0
             camera.saturation = 0
             camera.brightness = 50
 
