f67a83917a375b17b4a193fe91f35fd1811a031f
lucas <catabriga90@gmail.com>
2021-01-24 14:52:51 +0100

    debug sleep

--- PiCamRunner.py
+++ PiCamRunner.py
@@ -14,7 +14,7 @@ class PiCamRunner():
 
     def run(self):
         with picamera.PiCamera() as camera:
-            camera.resolution = self.size
+            camera.resolution = (1640, 1232)
             camera.rotation = self.rotation
             camera.framerate_range = (0.0333, 30)
             camera.shutter_speed = 3000000
@@ -24,6 +24,7 @@ class PiCamRunner():
             camera.contrast = 0
             camera.saturation = 0
             camera.brightness = 50
+            camera.annotate_foreground = picamera.Color(r=1.0, g=0.0, b=0.0)
 
             raw_capture = picamera.array.PiRGBArray(camera, size=camera.resolution)
 
