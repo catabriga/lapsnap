bead92cc6681ebc9732370ca5d4169638a21837f
Lucas Catabriga <catabriga90@gmail.com>
2021-01-24 10:16:59 +0100

    change to capture

--- PiCamRunner.py
+++ PiCamRunner.py
@@ -1,6 +1,7 @@
 import picamera
 import picamera.array
 import time
+import datetime
 
 class PiCamRunner():
     def __init__(self, timelapse_period, timelapse_callback, size, rotation, framerate):
@@ -11,26 +12,47 @@ class PiCamRunner():
         self.rotation = rotation
 
 
+    # def run(self):
+    #     camera = picamera.PiCamera()
+    #     camera.resolution = self.size
+    #     camera.framerate = self.framerate
+    #     camera.rotation = self.rotation
+
+    #     raw_capture = picamera.array.PiRGBArray(camera, size=camera.resolution)
+
+    #     # subtract period so that an image is taken as soon as started
+    #     last_time = time.time() - self.timelapse_period
+
+    #     for frame in camera.capture_continuous(raw_capture, format="rgb", use_video_port=True):
+
+    #         current_time = time.time()
+
+    #         image = frame.array
+
+    #         if(current_time - last_time > self.timelapse_period):
+    #             self.timelapse_callback(image)
+    #             last_time = current_time
+
+    #         raw_capture.truncate(0)
+
     def run(self):
-        camera = picamera.PiCamera()
-        camera.resolution = self.size
-        camera.framerate = self.framerate
-        camera.rotation = self.rotation
+        with PiCamera() as camera:
+            camera.resolution = self.size
+            camera.framerate = self.framerate
+            camera.rotation = self.rotation
+            camera.annotate_text = str(datetime.datetime.now())
 
-        raw_capture = picamera.array.PiRGBArray(camera, size=camera.resolution)
+            raw_capture = picamera.array.PiRGBArray(camera, size=camera.resolution)
 
-        # subtract period so that an image is taken as soon as started
-        last_time = time.time() - self.timelapse_period
+            last_time = time.time()
 
-        for frame in camera.capture_continuous(raw_capture, format="rgb", use_video_port=True):
-
-            current_time = time.time()
-
-            image = frame.array
-
-            if(current_time - last_time > self.timelapse_period):
+            while(True):
+                camera.annotate_text =
+                camera.capture(raw_capture, format="rgb", use_video_port=False)
+                image = frame.array
                 self.timelapse_callback(image)
-                last_time = current_time
+                raw_capture.truncate(0)
 
-            raw_capture.truncate(0)
-
+                current_time = time.time()
+                sleep_time = max(self.timelapse_period - (current_time - last_time), 0)
+                time.sleep(sleep_time)
